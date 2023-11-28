26c0995e2bde11d4e3c985ff9f6494ecfb64a994
Yazzz28 <yacinemennaa@gmail.com>
2023-11-28 17:06:49 +0100

    update quete - 05

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -8,10 +8,17 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
 {
-    #[Route('/program/{page}',methods: ['GET'], requirements: ['page'=>'\d+'], name:'program_list')]
-    public function index(int $page = 1): Response
+    #[Route('/program/',methods: ['GET'], name:'program_index')]
+    public function index(): Response
     {
         return $this->render('program/index.html.twig', [
+            'website' => 'Wild Series',
+         ]);
+    }
+    #[Route('/program/{page}',methods: ['GET'], requirements: ['page'=>'\d+'], name:'program_{page}')]
+    public function show(int $page = 1): Response
+    {
+        return $this->render('program/show.html.twig', [
             'website' => 'Wild Series',
             'page' => $page,
          ]);
